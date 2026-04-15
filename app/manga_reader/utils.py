26b054ebc6cc34231e6d62564f1cc5535f928a9a
Minh Hieu <minhhieunn2000@gmail.com>
2026-04-15 13:13:44 +0700

    update requirements and fix utils

--- app/manga_reader/utils.py
+++ app/manga_reader/utils.py
@@ -11,4 +11,4 @@ async def extract_search_vrf_async(search_query):
 
 
 async def get_mangafire_images_url(chapter_url):
-    return await client.get_chapter_ajax_url.remote.aio('https://mangafire.to/read/test-flight-girlss.k28w/en/chapter-1')
+    return await client.get_chapter_ajax_url.remote.aio(chapter_url)
